1ea9782db32adc6bf737879031474e4fb6f7d14b
Uzmakh <urbazoha@gmail.com>
2023-10-02 12:09:16 +0500

    Initialized the React Project and set SPA routing

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* Jsx which is not similar to html */}
+      <h1 className='text-warning'>Bootstrap CDN</h1>
+      Navbar
+    </>
+  )
+}
+
+export default App
